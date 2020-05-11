c8229ec227948aea8fbea0949a0b62c153302ddf
Pablolo <pablo_olondriz@hotmail.com>
2020-05-11 17:57:43 +0200

    refactoring homepage code

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import { Route, Switch } from 'react-router-dom';
+import axios from 'axios';
 
 import AnonRoute from './components/AnonRoute';
 import PrivateRoute from './components/PrivateRoute';
+import NavBar from './components/Navbar';
 
-import Homepage from './views/Homepage';
+import HomePage from './views/Homepage';
 import Protected from "./views/Protected";
 import LoginWithAuth from './views/Login';
 import SignupWithAuth from './views/Signup';
@@ -14,21 +16,57 @@ import AuthProvider from './context/authContext';
 
 import './App.css';
 
+const STATUS = {
+  LOADING: "LOADING",
+  LOADED: "LOADED",
+  ERROR: "ERROR",
+};
+
 class App extends Component {
+  state = {
+    cars: [],
+    error: undefined,
+    status: STATUS.LOADING,
+  }
+
+  componentDidMount = () => {
+    axios.get(`${process.env.REACT_APP_BACKEND_URI}/cars`)
+    .then((response) => {
+      console.log(response.data);
+      this.setState({
+        cars: response.data,
+        status: STATUS.LOADED,
+      })
+    })
+    .catch((error) => {
+      this.setState({
+        error: error.name,
+        status: STATUS.ERROR,
+      })
+    })
+  }
+
   render() {
-    return (
-      <AuthProvider>
-        <div>
-          <Homepage />
-          <Switch>
-            <Route exact path={'/cars'} component={Cars} />
-            <AnonRoute exact path={'/login'} component={LoginWithAuth}/>
-            <AnonRoute exact path={'/signup'} component={SignupWithAuth}/>
-            <PrivateRoute exact path={"/protected"} component={Protected} />
-          </Switch>
-        </div>
-      </AuthProvider>
-    );
+    const { cars, status, error } = this.state;
+    switch (status) {
+      case STATUS.LOADING:
+        return <div>Loading...</div>
+      case STATUS.LOADED:
+        return <AuthProvider>
+                <div>
+                  <NavBar />
+                  <Switch>
+                    <Route exact path='/' component={HomePage}/>
+                    <Route exact path={'/cars'} component={Cars} />
+                    <AnonRoute exact path={'/login'} component={LoginWithAuth}/>
+                    <AnonRoute exact path={'/signup'} component={SignupWithAuth}/>
+                    <PrivateRoute exact path={"/protected"} component={Protected} />
+                  </Switch>
+                </div>
+              </AuthProvider>
+      case STATUS.ERROR:
+        return <div>{error}</div>
+    }
   }
 }
 
--- src/views/Homepage.js
+++ src/views/Homepage.js
@@ -1,14 +1,13 @@
 import React, { Component } from 'react';
 
-import NavBar from '../components/Navbar';
 import Search from '../components/SearchBar';
 
 class Homepage extends Component {
   render() {
     return (
       <div>
-        <NavBar />
         <Search />
+        Homepage
       </div>
     );
   }

b5439ddf3c7cd32c427d5d49587802ae8f5d111e
Pablolo <pablo_olondriz@hotmail.com>
2020-05-12 16:16:07 +0200

    car details page working

---
+++ src/components/BookingForm.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class BookingForm extends Component {
+  render() {
+    return (
+      <div>
+        <p>Booking Start</p>
+        <p>Booking End</p>
+        <button>Checkout</button>
+      </div>
+    );
+  }
+}
+
+export default BookingForm;
--- src/views/CarDetails.js
+++ src/views/CarDetails.js
@@ -1,11 +1,41 @@
 import React, { Component } from 'react';
+import BookingForm from '../components/BookingForm';
 
 class CarDetails extends Component {
+  
+  cardetail = () => {
+    const { cars } = this.props;
+    console.log(cars);
+    const carId = this.props.match.params.id;
+    return cars.map((car, index) => {
+      if (car._id === carId) {
+        return <div key={index}>
+          <img src={process.env.REACT_APP_BACKEND_URI + car.image} alt={car.carSpecs.make + car.carSpecs.model}/>
+          <p>{car.carSpecs.make} {car.carSpecs.model}</p>
+          <div>
+            <p>Year {car.carSpecs.year}</p>
+            <p>{car.transmission} Transmission</p>
+          </div>
+          <div>
+            <p>{car.dailyPrice}€/day</p>
+            <BookingForm />
+          </div>
+          <p>Hosted by</p>
+          <div>
+            <img src={process.env.REACT_APP_BACKEND_URI + '/images/misc/default-avatar.png'} alt=""/>
+            <p>{car.owner}</p>
+          </div>
+          <p>{car.description}</p>
+        </div>
+      }
+    })
+  }
+  
   render() {
-    console.log(this.props);
     return (
       <div>
         <h1>Each Car Detail</h1>
+        {this.cardetail()}
       </div>
     );
   }
